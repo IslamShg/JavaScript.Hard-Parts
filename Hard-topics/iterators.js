f492642dd798ecfd9d20ee26fe948937244fcf4b
Islam Shagaew <islamshg@mail.ru>
2021-12-05 11:38:08 +0300

    iterators update

--- Hard-topics/iterators.js
+++ Hard-topics/iterators.js
@@ -10,9 +10,30 @@ const createFC = (arr) => {
 const returnNextEl = createFC([1, 2, 3, 4])
 const returnNextEl2 = createFC([1, 2, 3, 4])
 
+// console.log(returnNextEl2())
 
-console.log(returnNextEl())
-console.log(returnNextEl())
-console.log(returnNextEl())
-console.log(returnNextEl())
-console.log(returnNextEl2())
+// make non iterable object iterable
+const personProps = {
+  name: "Islam",
+  surname: "Shagaev",
+  age: 18,
+  [Symbol.iterator]() {
+    const thisVal = this
+    const keys = Object.keys(this)
+    let i = 0
+    return {
+      next() {
+        if (i < keys.length) {
+          const returnObj = { value: thisVal[keys[i]], done: false }
+          i++
+          return returnObj
+        }
+        return { done: true }
+      },
+    }
+  },
+}
+
+for (let prop of personProps) {
+  console.log(prop)
+}
